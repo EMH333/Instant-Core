dc2d733416eb4f83ee55743940638ebbbb175656
Ethan <ethohampton@gmail.com>
2017-06-19 16:34:19 -0700

    Add Question DAO

---
+++ backend/src/main/java/com/ethohampton/instant/Objects/QuestionDAO.java
@@ -0,0 +1,19 @@
+package com.ethohampton.instant.Objects;
+
+import com.ethohampton.instant.Authentication.userManagment.BaseDAO;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by ethohampton on 6/19/17.
+ * DAO for questions
+ */
+
+public class QuestionDAO extends BaseDAO<Question> {
+    static final Logger LOG = LoggerFactory.getLogger(QuestionDAO.class);
+
+    public QuestionDAO() {
+        super(Question.class);
+    }
+}
